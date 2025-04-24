416cbf74b24fdee580e92b54d9b760536d9873ac
Bryan Morales <mor23283@uvg.edu.gt>
2025-04-23 23:03:51 -0600

    Creación del proyecto

---
+++ Robotic_Arm/Robotic_Arm/main.c
@@ -0,0 +1,18 @@
+/*
+ * Robotic_Arm.c
+ *
+ * Created: 23/04/2025 23:03:32
+ * Author : bsmor
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
