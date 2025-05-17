36e3011c46044fcae38b2cafca6e9cf1205af1f1
Bryan Morales <mor23283@uvg.edu.gt>
2025-05-16 20:56:02 -0600

    Complementos, lógica terminada
    Se terminó de agregar comentarios, se modificó la parte de comunicación USART debido a que presentaba problemas y se agregaron los complementos

--- Robotic_Arm/Robotic_Arm/main.c
+++ Robotic_Arm/Robotic_Arm/main.c
@@ -31,12 +31,16 @@ uint8_t pasos = 0;				// Pasos ejecutados
 # define NUM_MAX_PASOS 5		// Cantidad máxima de posiciones
 uint8_t cant_posiciones;		// Cantidad de posiciones guardadas
 uint8_t modo_repro_iniciado = 0;// Conocer si finalizo de reproducir
+
+uint8_t filtro_angulos[4] = {0}; // Un filtro por cada canal ADC (3-6)
 //************************************************************************************
 // Function prototypes
 void setup();
 
 void writeEPROM(uint8_t dato, uint8_t direccion);
 uint8_t readEPROM(uint8_t direccion);
+void enviar_char_UART(char c);
+void enviar_num_UART(uint8_t num);
 
 void manual();
 void reproducir_data();
@@ -116,6 +120,36 @@ uint8_t readEPROM(uint8_t direccion){
 	return EEDR;
 }
 
+
+void enviar_num_UART(uint8_t num) {
+	// Caso es pecial con el número 0 ya que no entra en la lógica 
+	if (num == 0) {
+		while (!(UCSR0A & (1 << UDRE0))); // Espera a que el registro de datos este vacío
+		UDR0 = '0';
+		return;
+	}
+
+	char digits[3]; // Hasta 3 dígitos para 0-180
+	uint8_t i = 0;
+	//Convierte de número a ascii para poder envíarlo correctamente 
+	while (num > 0) {
+		digits[i++] = (num % 10) + '0'; // % obtiene el último dígito del valor para poder ir guardando en el arreglo díjito 
+		num /= 10;
+	}
+
+	for (int8_t j = i - 1; j >= 0; j--) { // Recorre los dígitos en orden inverso para enviarlos correctamente.
+		while (!(UCSR0A & (1 << UDRE0)));
+		UDR0 = digits[j];
+	}
+}
+
+
+void enviar_char_UART(char c) {
+	while (!(UCSR0A & (1 << UDRE0)));
+	UDR0 = c; // Escribir un caracter para separar ángulos
+}
+
+
 void manual(){
 	//Mostrar el modo actual 
 	PORTB |= (1 << PORTB3);
@@ -147,7 +181,6 @@ void manual(){
 		cant_posiciones++; // Aumento posiciones
 	}
 }
-
 
 void reproducir_data() {
 	//Mostrar en los leds el modo actual
@@ -181,7 +214,6 @@ void reproducir_data() {
 }
 
 
-
 void escribir_angulo(){
 	PORTB |= (1 << PORTB3) | (1 << PORTB4); // Ambos LEDs
 	//Reinicio variables de otros modos (redundante)
@@ -193,6 +225,7 @@ void escribir_angulo(){
 	
 }
 
+
 //************************************************************************************
 // Interrupt subroutines
 
@@ -202,106 +235,116 @@ ISR(ADC_vect) {
 	multiplexar_ADC = ADMUX & 0x0F;
 	ADC_value = ADCH; // Valor ADC de 8 bits
 	uint8_t angle;
+	uint8_t idx = multiplexar_ADC - 3; // índice 0 a 3
 	
 	// Canal ADC4 invertir valor debido a la conexión física
 	if (multiplexar_ADC == 4) {
 		ADC_value = 255 - ADC_value;
 	}
-
-	// Escalar el rango útil del potenciómetro (0-127 la mitad) -> 0-180° del servo
-	if (ADC_value <= 127) {
-		angle = (ADC_value * 180UL) / 127;
-		} else {
-		angle = 180;
+	
+	// Escalar ángulo según el canal
+	if (multiplexar_ADC == 3 || multiplexar_ADC == 4 || multiplexar_ADC == 5) {
+		if (ADC_value <= 127) {
+			angle = (ADC_value * 180UL) / 127; // UL = unsigned long, se coloca ya que puede desbordarse al momento de realizar la operación, entero sin signo de 32 bits
+			} else {
+			angle = 180;
+		}
+		} else if (multiplexar_ADC == 6) {
+		angle = (ADC_value * 180UL) / 255;
 	}
+	
+	//Filtro la señal
+	//Forma original
+	//filtro_angulo = alfa * angle + (1-alfa) * filtro_angulo // FIltro de media exponencial
+	// Para no usar flotantes ya que alfa debe de ser un número cercano a 0 pero diferente de 1 entonces uso porcentajes
+	// alfa = 0.1 ? usamos 10 y 90 como porcentaje (factor 100)
+	filtro_angulos[idx] = (angle * 10 + filtro_angulos[idx] * 90) / 100;
 
-	// Calcular el pulso PWM para el servo
+	// Calcular pulso PWM
 	uint16_t servo_pulse;
 
-	switch(multiplexar_ADC) {
-		case 3: // Canal ADC3 -> Servo 1
-		servo_pulse = SERVO_MIN + (angle * (SERVO_MAX - SERVO_MIN)) / 180;
-		OCR1B = (uint16_t)servo_pulse;
-		ADMUX = (ADMUX & 0xF0) | 4; // Pasar al sigueinte ADC
+	switch (multiplexar_ADC) {
+		case 3: // Servo 1 (base)
+		servo_pulse = SERVO_MIN + (filtro_angulos[idx] * (SERVO_MAX - SERVO_MIN)) / 180;
+		OCR1B = servo_pulse;
+		ADMUX = (ADMUX & 0xF0) | 4;
 		break;
 
-		case 4: // Canal ADC4 -> Servo 2 (inversión aplicada arriba)
-		servo_pulse = SERVO_MIN_2 + (angle * (SERVO_MAX_2 - SERVO_MIN_2)) / 180;
-		OCR1A = (uint16_t)servo_pulse;
+		case 4: // Servo 2 (antebrazo)
+		servo_pulse = SERVO_MIN_2 + (filtro_angulos[idx] * (SERVO_MAX_2 - SERVO_MIN_2)) / 180;
+		OCR1A = servo_pulse;
 		ADMUX = (ADMUX & 0xF0) | 5;
 		break;
 
-		case 5: // Canal ADC5 -> Servo 3
-		servo_pulse = SERVO_MIN_3 + (angle * (SERVO_MAX_3 - SERVO_MIN_3)) / 180;
-		OCR0A = (uint16_t)servo_pulse;
+		case 5: // Servo 3 (brazo)
+		servo_pulse = SERVO_MIN_3 + (filtro_angulos[idx] * (SERVO_MAX_3 - SERVO_MIN_3)) / 180;
+		OCR0A = servo_pulse;
 		ADMUX = (ADMUX & 0xF0) | 6;
 		break;
 
-		case 6: // Canal ADC6 -> Servo 4
-		servo_pulse = SERVO_MIN_4 + (angle * (SERVO_MAX_4 - SERVO_MIN_4)) / 180;
-		OCR0B = (uint16_t)servo_pulse;
-		ADMUX = (ADMUX & 0xF0) | 3; // Regresar al primer ADC (ADC3)
+		case 6: // Servo 4 (garra)
+		servo_pulse = SERVO_MIN_4 + (filtro_angulos[idx] * (SERVO_MAX_4 - SERVO_MIN_4)) / 180;
+		OCR0B = servo_pulse;
+		ADMUX = (ADMUX & 0xF0) | 3;
 		break;
 	}
 
 	ADCSRA |= (1 << ADSC); // Iniciar nueva conversión
 }
 
-
-
 #define MAX_BUFFER 20
-volatile char buffer[MAX_BUFFER]; // El buffer se encarga de contener los valores recibidos
-volatile uint8_t index = 0; // Tiene el número de caracteres almacenados 
+volatile char buffer[MAX_BUFFER];
+volatile uint8_t index = 0; // Ïndice del bufer para ir guardando los caracteres recibidos 
 
 ISR(USART_RX_vect) {
-	
-	if (modo != 3) return; // Solo ejecutar 
-	
+	if (modo != 3) return; // Si no está en modo 3 ignora los valores recibidos y sale de la ISR
+
 	char recibido = UDR0;
 
-	// Cuando llega fin de línea (enter)
-	if (recibido == '\n' || recibido == '\r') {
+	if (recibido == '\n' || recibido == '\r') { // Si se recibe saldo de línea o retorno inidca que se finalizó la transmisión
+		buffer[index] = '\0'; // Terminación de la línea
 		
-		buffer[index] = '\0';  // Terminar cadena cuando se manda un "enter" el cual es el valor nulo 
-
-		// Variables temporales
-		uint8_t valores[4] = {0};
+		// En esta parte se convierte el valor ascci (texto) a un número
+		uint8_t valores[4] = {0}; //Arreglo para guardar los 4 caracteres de los ángulos (180,)
 		uint8_t val_idx = 0;
-		uint16_t temp = 0;
+		uint16_t temp = 0; // Se usa para acumular dígitos
 
-		for (uint8_t i = 0; i <= index; i++) { // Esto lo que hace es barrer todo el string
-			char c = buffer[i]; // Extrae caracter actual y lo guarda en c 
+		for (uint8_t i = 0; i <= index; i++) { // recorre cada caractér del buffer 
+			char c = buffer[i]; // Agarra el valor actual y pasa a procesarlo 
 
-		// Iniciamos convirtiendo los caracteres en números, del 0 al 9 
-			if ((c >= '0') && (c <= '9')) { 
-				temp = temp * 10 + (c - '0');
+			if (c >= '0' && c <= '9') { // Si el caractér está entre 0 y 9
+				temp = temp * 10 + (c - '0'); // Va barriendo los dígitos y multiplicarlos por 10 para hacer unidad, decena y centena
 			}
-		// Manejamos el otro caso, significa que terminó de leer el número 
-			else if (c == ',' || c == '\0') {
-				
-		// Doble verifiación, si la cadena no ha superado los 4 números y si el valor es menor a los 180°
+			else if (c == ',' || c == '\0') { // Si hay coma o \0 termina el dígito y lo guarda en el arreglo valores 
 				if (val_idx < 4) {
-					if (temp > 180) temp = 180; // Límite máximo para servo
-					valores[val_idx++] = temp; // Guarda el número en el nuevo arreglo e incrementa para guardar el siguiente número 
-					temp = 0; // Reinicia "temp" para empezar a construir y guardar el nuevo número
+					if (temp > 180) temp = 180; // Limita el ángulo a 180 por que ese valor trabaja el servo
+					valores[val_idx++] = temp; 
+					temp = 0; // Reinicia para construir nuevo número 
 				}
 			}
 		}
 
-		// Convertir a OCR valores (mismo en en el MUX)
+		// Aplicar valores a servos, la lógica es igual que en el ADC_vect
+		//PWM = SERVO_MIN + (valor * ancho_pwm) / 180
 		OCR1B = SERVO_MIN + (valores[0] * (SERVO_MAX - SERVO_MIN)) / 180;
 		OCR1A = SERVO_MIN_2 + (valores[1] * (SERVO_MAX_2 - SERVO_MIN_2)) / 180;
 		OCR0A = SERVO_MIN_3 + (valores[2] * (SERVO_MAX_3 - SERVO_MIN_3)) / 180;
 		OCR0B = SERVO_MIN_4 + (valores[3] * (SERVO_MAX_4 - SERVO_MIN_4)) / 180;
 
-		index = 0; // Reiniciar buffer
+		// Enviar feedback por UART
+		enviar_num_UART(valores[0]);
+		enviar_char_UART(',');
+		enviar_num_UART(valores[1]);
+		enviar_char_UART(',');
+		enviar_num_UART(valores[2]);
+		enviar_char_UART(',');
+		enviar_num_UART(valores[3]);
+		enviar_char_UART('\n');
+
+		index = 0;
 	}
-	else { 
-		// Esta parte es como un recolector de catacteres 
-		// Acumular caracteres si hay espacio
-		if (index < MAX_BUFFER - 1) {
-			buffer[index++] = recibido;
-		}
+	else {
+		buffer[index++] = recibido; //Si no se recibió un \n o \r guarda el carácter en el buffer y aumenta el índice.
 	}
 }
 
@@ -310,11 +353,11 @@ ISR(PCINT1_vect) {
 	// Verificar estado actual de los botones
 	if (!(PINC & (1 << PORTC0))) {  // Si PC0 está presionado (pull-up)
 		modo++;
-		if (modo >= cant_modo){
+		if (modo >= cant_modo){ // Reiniciar a 1 el modo si se llegó al últmio modo
 			modo = 1;
 		}
 	} else if (!(PINC & (1 << PORTC1))){
-		action_button = 1;
+		action_button = 1; // Activar bandera de acción
 	}
 }
 
